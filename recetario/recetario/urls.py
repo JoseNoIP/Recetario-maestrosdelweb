03c068566280825c4c3e7e48fd8baef5ef781624
José Norberto <Ibarra Plascencia>
2013-07-06 21:56:05 -0600

    Tema 6: Las Vistas
    Proyecto en estado funcional hasta el paso 6 del curso de Django de
    Maestros del web

--- recetario/recetario/urls.py
+++ recetario/recetario/urls.py
@@ -6,7 +6,11 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    #url(r'^$', 'principal.views.lista_bebidas'),
+    url(r'^$', 'principal.views.inicio'),
+    url(r'^usuarios/$', 'principal.views.usuarios'),
+    url(r'^sobre/$', 'principal.views.sobre'),
+    url(r'^recetas/$', 'principal.views.lista_recetas'),
+    url(r'^receta/(?P<id_receta>\d+)$','principal.views.detalle_receta'),
     # url(r'^recetario/', include('recetario.foo.urls')),
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
